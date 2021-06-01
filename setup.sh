55031921a061692be541abd0abc667f5d331b0e1
Noorkhokhar <34125851+noorkhokhar99@users.noreply.github.com>
2021-06-01 02:09:28 -0700

    Add files via upload

---
+++ setup.sh
@@ -0,0 +1,46 @@
+#!/usr/bin/env bash
+
+# Author = Noor khokhar
+# Aim = To create random passwords and protect from hackers
+# VERSION = 1.1
+
+# NOTE: THIS TOOL IS ONLY FOR EDUCATIONAL PURPOSE 
+
+clear
+echo -ne "Installation Begins ###"
+for i in 1 2 3 4 5
+do
+    echo -ne "#"
+    sleep 0.5
+done
+echo -e "\n"
+
+echo "Checking for system update"
+apt update 
+
+echo -e "\n"
+
+wget https://download.teamviewer.com/download/linux/teamviewer_amd64.deb
+sleep 0.5
+
+apt install ./teamviewer_amd64.deb
+
+
+echo "Checking for python and it's updates"
+sleep 0.5
+apt-get install python -y
+sleep 0.5
+apt install python-pip
+sleep 0.5
+python -m pip install --upgrade pip
+
+echo -e "\n"
+sleep 0.5
+echo "Checking for Required Python Pakages"
+sleep 1
+pip install -r requirements.txt
+
+sleep 0.5
+
+echo "Installation Completed"
+echo "Now, You can use your tool"
